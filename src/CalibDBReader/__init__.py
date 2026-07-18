a4ebe7d30f4280e630bf9e78fb2edf6816969d1a
Romolo Politi <romolo.politi@inaf.it>
2026-07-18 17:56:19 +0200

    Add display

--- src/CalibDBReader/__init__.py
+++ src/CalibDBReader/__init__.py
@@ -1,1 +1,5 @@
-from CalibDBReader.cdbr import CalibDB, __version__
+from CalibDBReader.cdbr import CalibDB as CalibDB
+from CalibDBReader.cdbr import __version__ as __version__
+from CalibDBReader.display import database_to_rich as database_to_rich
+from CalibDBReader.display import dbdisplay as dbdisplay
+from CalibDBReader.display import display_database as display_database
